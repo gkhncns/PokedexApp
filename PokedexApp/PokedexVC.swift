252dbee30f122ed915f36d95d8c2c31e57e6e0da
Gokhan <gokhan@gokhans-mbp.gateway>
2017-07-01 14:27:50 +1000

    added collection view delegate, data source, flow layout and corner radius for pokecell

--- PokedexApp/PokedexVC.swift
+++ PokedexApp/PokedexVC.swift
@@ -8,12 +8,60 @@
 
 import UIKit
 
-class PokedexVC: UIViewController {
+class PokedexVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var pokemonsCollection: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        pokemonsCollection.delegate = self
+        pokemonsCollection.dataSource = self
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
+            
+            let pokemon = Pokemon(id: indexPath.row+1, name: "Pokemon")
+            cell.configureCell(pokemon: pokemon)
+            
+            return cell
+            
+        } else {
+            
+            return UICollectionViewCell()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        // Forward to related page
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 30
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 85, height: 85)
     }
 
 }
+
+
+
+
+
+
+
+
 
