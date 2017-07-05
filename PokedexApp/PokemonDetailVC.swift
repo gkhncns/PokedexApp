107682d9dcbb101b5ba266f74fdb49ff54dabb1d
Gokhan <gokhan@gokhans-mbp.gateway>
2017-07-05 21:37:02 +1000

    Added labels labels and image views to detail view

--- PokedexApp/PokemonDetailVC.swift
+++ PokedexApp/PokemonDetailVC.swift
@@ -10,14 +10,18 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
     
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var pokemonNameLabel: UILabel!
 
     var pokemon: Pokemon!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        label.text = pokemon.name
+        pokemonNameLabel.text = pokemon.name.capitalized
     }
-
+    
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
